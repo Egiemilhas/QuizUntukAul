ddb6f9526d6ace7434f77f252f5880e9c45aea49
Egiemilhas <egiemilhas@gmail.com>
2023-12-15 21:38:18 +0700

    Menambahkan proyek kuis HTML

---
+++ quiz.js
@@ -0,0 +1,31 @@
+function submitQuiz() {
+  const answers = {
+    question1: document.getElementById('question1').value.trim(),
+    question2: document.getElementById('question2').value.trim(),
+    question3: document.getElementById('question3').value.trim(),
+  };
+
+  const correctAnswers = {
+    question1: 'b',
+    question2: 'b',
+    question3: 'a',
+  };
+
+  let score = 0;
+
+  for (const question in answers) {
+    if (answers[question].toLowerCase() === correctAnswers[question]) {
+      score++;
+    }
+  }
+
+  displayResult(score);
+}
+
+function displayResult(score) {
+  const resultContainer = document.getElementById('result-container');
+  const quizResult = document.getElementById('quiz-result');
+
+  quizResult.textContent = `Anda mendapatkan ${score} dari 3 pertanyaan benar! Selamat Anniversary! 🎉`;
+  resultContainer.style.display = 'block';
+}
